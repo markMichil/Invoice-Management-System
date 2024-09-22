059d72037fce0fca72c3b2ea3b3472ac20a76d0e
markMichil <mark2michil@gmail.com>
2024-09-22 17:46:02 +0300

    migration_seeder_models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['ADMIN', 'EMPLOYEE']);
+            $table->softDeletes(); // Adds deleted_at field for soft delete
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'ADMIN'
+        ]);
+
+        User::create([
+            'name' => 'Employee User',
+            'email' => 'employee@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'EMPLOYEE'
+        ]);
+    }
+}
